acc581a469dc12241615e144dd04985ca6a27493
fabioaufinger <43215745+fabioaufinger@users.noreply.github.com>
2019-01-26 17:50:00 +0100

    Add files via upload

---
+++ Software/Test/README.md
@@ -0,0 +1,1 @@
+![Objekterkennung Programmaufbau](https://github.com/ampxtreme/Gruppe-2---Mars-Rover-Challenge/blob/master/Software/Objekterkennung/objekterkennung_programm.jpg)
---
+++ Software/Test/conf.py
@@ -0,0 +1,34 @@
+# Config Modul
+
+debug=True
+
+#LEDs
+intLED = 1
+programmLED1=21
+programmLED2=20
+programmLED3=16
+programmLED4=26
+
+
+#Taster
+tasterDig1=19
+tasterDig2=13
+tasterStop=5
+tasterRun=6
+
+
+#SoftwareSwitch Ports
+swSwitchGPIO1=1
+swSwitchGPIO2=2
+swSwitchGPIO3=3
+swSwitchRead=4
+swSwitchWrite=5
+
+addrSonicL=(1, 0, 0)
+addrSonicR=(1, 1, 1)
+
+
+#Bildverabeitun
+camCropLT=(180, 220, 100, 240)
+camColorLB = (12,100,100)
+camColorUB = (32,255,255)

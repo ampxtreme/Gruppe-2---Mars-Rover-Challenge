b8bde2d6e16c1574fb0fd27014ca9153a469a055
falk <githup.reg@hda1.de>
2019-02-02 10:59:19 +0100

    Add files via upload

--- Software/conf.py
+++ Software/conf.py
@@ -1,31 +1,93 @@
-# Config Modul
+# Config Modul 02.02. 11:00
 
 debug=True
+pcb=False # PCB Ports
 
-#LEDs
+#LEDs solllen über I1c
 intLED = 1
-programmLED1=21
-programmLED2=20
-programmLED3=16
-programmLED4=26
+programmLED1=21 # 
+programmLED2=20 # 
+programmLED3=16 # 
+programmLED4=26 #
+
+#I2C
+i2csSDA=8
+i2cSCL=9
+
+
+i2cP1=(0x21, 1, 1)# Grüne LEDs, Programmwahl
+i2cP2=(0x21, 1, 2)
+i2cP3=(0x21, 1, 3)
+i2cP4=(0x21, 1, 5)
+
+i2cGelb1=(0x21, 1, 0)
+i2cGelb2=(0x21, 1, 4)
+
+i2cGreen1=(0x21, 1, 6)
+i2cGreen2=(0x21, 1, 7)
+
+addrGyro=0x68  # via i2cdetect 
+
+#motors ic2 addr, Port, Pin Motor
+motor1P=(0x20, 0, 0)#on/off
+motor1Rot=(0x20, 1, 0)# rotation direction
+motor1Speed1=(0x20, 0, 6)# 4 Stufen 
+motor1Speed2=(0x20, 0, 7)# 4 Stufen 
+
+motor2P=(0x20, 0, 1)
+motor2Rot=(0x20, 1, 1)# rotation direction
+motor2Speed1=(0x20, 1, 6)# 4 Stufen
+motor2Speed2=(0x20, 1, 7)# 4 Stufen
+
+motor3P=(0x20, 0, 2)
+motor3Rot=(0x20, 1, 2)# rotation direction
+motor3Speed1=(0x21, 0, 0)# 4 Stufen
+motor3Speed2=(0x21, 0, 1)# 4 Stufen
+
+motor4P=(0x20, 0, 3)
+motor4Rot=(0x20, 1, 3)# rotation direction
+motor4Speed1=(0x21, 0, 2)# 4 Stufen
+motor4Speed2=(0x21, 0, 3)# 4 Stufen
+
+motor5P=(0x21, 0, 4)
+motor5Rot=(0x20, 1, 4)# rotation direction
+motor5Speed1=(0x21, 0, 4)# 4 Stufen
+motor5Speed2=(0x21, 0, 5)# 4 Stufen
+
+motor6p=(0x20, 0, 5)
+motor6Rot=(0x20, 1, 5)# rotation direction
+motor6Speed1=(0x21, 0, 6)# 4 Stufen
+motor6Speed2=(0x21, 0, 7)# 4 Stufen
+
 
 
 #Taster
 tasterDig1=19
 tasterDig2=13
-tasterStop=5
-tasterRun=6
+tasterStop=5  #pcb GPIO 21
+tasterRun=6   #pcb GPIO 22 //vmtl rot
+
+rotaryP1=(0, 0, 0, 0)
+rotaryGPIOs=(27, 28, 29, 25) # GPIO READ Ports (klein nach Groß)
 
 
-#SoftwareSwitch Ports
-swSwitchGPIO1=1
-swSwitchGPIO2=2
-swSwitchGPIO3=3
-swSwitchRead=23
-swSwitchWrite=24
+#SoftwareSwitch Ports Nr1
+swSwitchGPIO1=2 # 
+swSwitchGPIO2=3 #
+swSwitchGPIO3=4 #
+swSwitchRead=23 #pcb GPIO 0
+swSwitchWrite=24 #pcb GPIO 1
 
 addrSonicL=(1, 0, 0)
 addrSonicR=(1, 1, 1)
+addrSonicR=(1, 1, 1)
+
+#SoftwareSwitch Ports Nr2
+swSwitch2GPIO1=12 # 
+swSwitch2GPIO2=13 #
+swSwitch2GPIO3=14 #
+swSwitch2Read=10 #
+swSwitch2Write=11 #
 
 
 #Bildverabeitun
@@ -34,6 +96,14 @@ camColorLB = (12,100,100)
 camColorUB = (32,255,255)
 camDistance = 15
 
+#free GPIO
+#5-7
+#24
 
-#I2C Adresses
-addrGyro=0x68  # via i2cdetect
+pwm1=23
+pwm0=26
+
+#serial com 
+serialTX=15
+serialRX=16
+

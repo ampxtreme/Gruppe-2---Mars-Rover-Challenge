5f673afcbd6d566ae52176fd1d031e58ab608d34
falk <githup.reg@hda1.de>
2019-02-09 12:01:57 +0100

    Add files via upload

--- Software/Test/ModulTests/drives.py
+++ Software/Test/ModulTests/drives.py
@@ -2,30 +2,44 @@ import  sys
 sys.path.insert(0, '../../')
 import RPi.GPIO as GPIO
 import conf
+import time
 
 GPIO.setmode(GPIO.BCM)
 import drive
-
+print(drive.BANK0, drive.BANK1, drive.BANK2)
 
 print("init drives")
+
 drive.init()
 
-def leftTurn(time=5)
+def leftTurn(time=5):
     drive.drive("L", 50, 2, 0)
     drive.drive("R", 50, 2, 1)
     time.sleep(time)
     drive.stop()
 
 #0-5
+print(drive.BANK0, drive.BANK1, drive.BANK2)
 
 # Auskommentieren, was man testen mag!
-
+#drive.drive("L", 100,3)
+#drive.drive("R", 50,3)
 # mortot, speed lvl, richtung 0,1
-drive.setMotor(0, 2, 0 )
+#drive.setMotor(0, 2, 0 )
 
-#drive.test()
 
+
+print(drive.BANK0, drive.BANK1, drive.BANK2)
 #drive.testpwm()
 
-#leftTurn(1)
+drive.test()
 
+drive.testpwm()
+
+time.sleep(4)
+#leftTurn(1)
+drive.setSpeedLevel(0, 0)
+drive.PWMR.stop()
+drive.PWML.stop()
+
+GPIO.cleanup()
